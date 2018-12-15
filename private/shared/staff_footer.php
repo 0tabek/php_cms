cfaf7e5d18ede2a11423d626b71f52a814b4b689
Otabek Atabaev <oatabaev94@gmail.com>
2018-12-15 13:25:22 +0500

    Start

---
+++ private/shared/staff_footer.php
@@ -0,0 +1,6 @@
+<footer>
+  &copy; <?php echo date('Y')?> Project Name
+</footer>
+
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Project Name</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+
+    <h1>Project: Coming soon </h1>
+
+  </body>
+</html>

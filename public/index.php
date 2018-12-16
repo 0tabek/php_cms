73bd7aeb2b7666b9128909df867c203e613c6792
Otabek <oatabaev94@gmail.com>
2018-12-16 20:47:39 +0500

    Next is MySql

--- public/index.php
+++ public/index.php
@@ -2,13 +2,13 @@
 
 <html lang="en">
   <head>
-    <title>Project Name</title>
+    <title>Globe Bank</title>
     <meta charset="utf-8">
   </head>
 
   <body>
 
-    <h1>Project: Coming soon </h1>
+    <h1>Globe Bank: Coming Soon</h1>
 
   </body>
 </html>
